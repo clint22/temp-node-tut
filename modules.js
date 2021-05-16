4a10a3e19196ed3a0855d6accfdbe75d786c2ee6
clintpaul <clintpaul89@gmail.com>
2021-05-16 23:02:11 +0530

    initial commit

---
+++ modules.js
@@ -0,0 +1,8 @@
+const names = require('./names');
+const sayHi = require('./utils');
+console.log(names);
+const data = require('./alternative-flavor');
+require('./mind-grenade');
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
